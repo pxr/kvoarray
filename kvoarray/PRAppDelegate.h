04b971a34c2ce489c481e5f1d6c2d627420b726d
Paul Robinson <paul@elasticrat.com>
2014-04-16 19:36:59 -0700

    Initial Commit

---
+++ kvoarray/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  kvoarray
+//
+//  Created by Paul Robinson on 2014-04-16.
+//  Copyright (c) 2014 Elastic Rat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ kvoarray/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  kvoarray
+//
+//  Created by Paul Robinson on 2014-04-16.
+//  Copyright (c) 2014 Elastic Rat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
